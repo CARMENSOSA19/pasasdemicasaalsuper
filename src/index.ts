b76b706e24345fcbac4d0c204fa9c80ebe6a2c44
CARMENSOSA19 <104264514+CARMENSOSA19@users.noreply.github.com>
2022-04-28 19:26:32 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+console.log("avanzo hasta la esquina");
+console.log("giro a la izquierda");
+console.log("avanzo hasta la esquina");
+console.log("giro a la derecha");
+console.log("avanzo hasta la esquina");
+console.log("giro a la izquierda");
+console.log("llegue");
